79c270e0405be05b2fdad1eb660feb8a61bbdbe9
Kenneth Chiem <kc696@cornell.edu>
2023-12-22 14:44:30 -0500

    add styling to Services title and description

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -7,9 +7,9 @@
 <Navbar selected="services" home={false} />
 
 <div class="services-intro">
-	<h1>Our Services</h1>
+	<h1 class="title">Our Services</h1>
 
-	<p>
+	<p class="description">
 		We take care in the service we provide to ensure top quality results. Please take your time
 		looking over the services we provide.
 	</p>
@@ -103,5 +103,17 @@
 		&:after {
 			right: -2px;
 		}
+	}
+
+	.title {
+		font-size: 3rem;
+		margin: 0;
+		border-bottom: 3px solid #febecc;
+		margin-bottom: 3rem;
+	}
+
+	.description {
+		font-size: 1.1rem;
+		text-align: center;
 	}
 </style>
